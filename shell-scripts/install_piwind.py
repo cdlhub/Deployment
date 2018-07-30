ec81b77cca8b588ecb57a9de2896b68327314aee
Camille Daum-Lobko <camille@daum-lobko.com>
2018-07-30 11:58:20 -0400

    Update doc and options

--- shell-scripts/install_piwind.py
+++ shell-scripts/install_piwind.py
@@ -3,7 +3,33 @@
 
 """
 This script is used to install PiWind model to Flamingo server already
-provisionned with `Flamingo_Midtier_CalcBE.py` script.
+provisionned with `deploy_OASIS.py` script.
+
+You can use it directly on the OASIS server using `--local` option or 
+from your workstation using SSH.
+
+Example (you need a valid `config.ini` file):
+
+- Installing PiWind on AWS instance using SSH:
+
+python install_piwind.py --sshuser ubuntu \
+                         --host <oasis-server-ip> \
+                         --config config.ini \
+                         --key <aws-key-file.pem>
+                         --gituser <git-user> \
+                         --gitpassword <git-password> \
+                         --dockeruser <docker-user> \
+                         --dockerpassword <docker-password>
+                        
+- Installing PiWind from within the OASIS server. `install_piwind.py` and `install-piwind-template.sh`
+  must be uploaded first on the server:
+
+python install_piwind.py --local \
+                         --config config.ini \
+                         --gituser <git-user> \
+                         --gitpassword <git-password> \
+                         --dockeruser <docker-user> \
+                         --dockerpassword <docker-password>
 """
 
 import argparse
@@ -17,11 +43,11 @@ import sys
 
 parser = argparse.ArgumentParser(description='Install PiWind model on Flamingo server EC2 instance.')
 
-parser.add_argument("--user", action='store', dest='username', default='ubuntu', help='SSH username')
-parser.add_argument("--host", action='store', dest='hostname', required=True, help='Flamingo server public IP address')
+parser.add_argument("--sshuser", action='store', dest='username', default='ubuntu', help='SSH username (default: ubuntu)')
+parser.add_argument("--host", action='store', dest='hostname', required=False, help='Flamingo server public IP address (required for SSH)')
 parser.add_argument('--config', action='store', dest='config', default='config.ini', help='set INI configuration file name (default: config.ini)')
 parser.add_argument('--session', action='store', dest='session_profile', default='default', required=False, help='AWS profile to get credentials')
-parser.add_argument('--key', action='store', dest='key_name', required=True, help='AWS access key file name to access the instace')
+parser.add_argument('--key', action='store', dest='key_name', required=False, help='AWS access key file name to access the instace (required for AWS instance)')
 parser.add_argument('--gituser', action='store', dest='git_user', required=True, help='git user name')
 parser.add_argument('--gitpassword', action='store', dest='git_password', required=True, help='git user password')
 parser.add_argument('--dockeruser', action='store', dest='docker_user', required=True, help='docker user name')
@@ -72,7 +98,7 @@ if ( args.local ):
 
     subprocess.call(['sudo', tmp_script_name])
 
-    # os.remove(tmp_script_name)
+    os.remove(tmp_script_name)
     sys.exit(0)
 
 
@@ -88,11 +114,11 @@ ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(**ssh_connect)
 
-print("run install script on server...")
+print("running install script on server...")
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(script, get_pty=True)
 
 for line in iter(ssh_stdout.readline, ""):
     print(line)
-print('...finished')
+print('Done')
 
 ssh.close()
