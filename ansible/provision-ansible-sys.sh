8f587a6ad3588ea49fcf0c5590fb45d520bfa71a
Camille Daum-Lobko <camille@daum-lobko.com>
2019-03-22 14:37:56 +0000

    Add Ansible scripts from ~ Camille Daum-Lobko

---
+++ ansible/provision-ansible-sys.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo yum update -y
+
+# Python, pip, and boto
+sudo yum install epel-release -y --nogpgcheck
+sudo yum install python-pip -y
+sudo pip install --upgrade pip
+sudo pip install boto
+
+# Ansible
+sudo yum install ansible -y
