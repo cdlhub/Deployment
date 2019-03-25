836975f714ab9ead022d22b0ee6d74cec4d279b8
Camille Daum-Lobko <camille@daum-lobko.com>
2019-03-25 10:33:43 -0400

    Add wget -q option

--- vagrant/provision-deploy-sys.sh
+++ vagrant/provision-deploy-sys.sh
@@ -9,7 +9,7 @@ sudo yum install ansible -y
 export TERRAFORMF_VERSION="0.11.13"
 
 sudo yum install wget unzip -y
-sudo wget https://releases.hashicorp.com/terraform/${TERRAFORMF_VERSION}/terraform_${TERRAFORMF_VERSION}_linux_amd64.zip
+sudo wget -q https://releases.hashicorp.com/terraform/${TERRAFORMF_VERSION}/terraform_${TERRAFORMF_VERSION}_linux_amd64.zip
 sudo unzip -qq terraform_${TERRAFORMF_VERSION}_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 sudo rm -f terraform_${TERRAFORMF_VERSION}_linux_amd64.zip
