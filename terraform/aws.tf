19d9ee6af3120101fffd6b457009fe2a2af72ffa
Camille Daum-Lobko <camille@daum-lobko.com>
2019-03-25 11:07:18 -0400

    Add variables.tf

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,6 +1,10 @@
 provider "aws" {
   profile = "cdl_tftuto"
-  region = "ca-central-1"
+  region = "${var.region}"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.oasis_aws.public_ip} > ip_address.txt"
+  }
 }
 
 resource "aws_instance" "oasis_aws" {
