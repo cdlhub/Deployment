0c1a3bbe14aaaf44bb4b87105ac21b37421a8349
Camille Daum-Lobko <camille@daum-lobko.com>
2019-03-25 10:23:59 -0400

    Rename anisble-sys to deploy-sys

--- vagrant/provision-deploy-sys.sh
+++ vagrant/provision-deploy-sys.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-export TERRAFORMF_VERSION="0.11.13"
-
 sudo yum update -y
 
-sudo yum install wget unzip -y
+# Ansible
 sudo yum install ansible -y
 
+## Terraform
+export TERRAFORMF_VERSION="0.11.13"
+
+sudo yum install wget unzip -y
 sudo wget https://releases.hashicorp.com/terraform/${TERRAFORMF_VERSION}/terraform_${TERRAFORMF_VERSION}_linux_amd64.zip
 sudo unzip -qq terraform_${TERRAFORMF_VERSION}_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
